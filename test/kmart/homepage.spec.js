328aca8bdae55c576fbcf74cfc2c04b2549b4760
Sergey Troyeglazov <troyeglazov.s.a@gmail.com>
2020-04-28 21:38:53 -0400

    refactor

--- test/kmart/homepage.spec.js
+++ test/kmart/homepage.spec.js
@@ -3,8 +3,8 @@ import Homepage from "./pages/Homepage";
 describe('test', ()=> {
   it('should open kmart page', ()=> {
     Homepage.open("https://www.kmart.com");
-bbbbb
-  })bbb
+
+  })
 
 
 })
--- test/kmart/pages/Base.js
+++ test/kmart/pages/Base.js
@@ -1,5 +1,4 @@
 export default class Base {
-  open(path) {
-    browser.url(path);
-  }
+  open(path){}
+
 }
