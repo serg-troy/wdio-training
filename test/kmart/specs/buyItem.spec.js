73027eeb92051fe413af189e263c341ef457c790
Sergey Troyeglazov <troyeglazov.s.a@gmail.com>
2020-05-22 23:36:30 -0400

    README

--- test/kmart/specs/buyItem.spec.js
+++ test/kmart/specs/buyItem.spec.js
@@ -1,17 +1,16 @@
 import Homepage from '../pages/Homepage';
 import ProductPage from '../pages/ProductPage';
 
-describe('test', () => {
+describe('Test KMart', () => {
   it('should open kmart page and check that page is correct', () => {
     Homepage.open();
     browser.maximizeWindow();
     expect(browser.getTitle()).contains('Kmart');
   });
 
-  it('sho', () => {
+  it('should find PS4 Pro and add it to cart', () => {
     Homepage.searchFor('ps4 pro');
     $('.card-image').click();
     ProductPage.addToCartBtn.click();
-    browser.pause(5000);
   });
 });
