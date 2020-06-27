10f7842c84042668f509f49dacb1aa900596cdda
Sergey Troyeglazov <troyeglazov.s.a@gmail.com>
2020-06-26 21:51:53 -0400

    sandbox practice

--- test/sandbox.spec.js
+++ test/sandbox.spec.js
@@ -1,10 +1,5 @@
 describe('SANDBOX', () => {
   it('should pass', () => {
-    browser.url('https://reg.usps.com/entreg/RegistrationAction_input');
-    browser.pause(2000)
-    $('#slanguage').selectByIndex(1);
-    browser.pause(4000)
-    $('#slanguage').selectByIndex(0);
-    browser.waitUntil(() => $('#slanguage').getText() === "English")
+
   });
 });
