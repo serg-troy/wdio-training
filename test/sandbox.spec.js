87fe1c97e5538a29917a855b58b98d79d0d74208
Sergey Troyeglazov <troyeglazov.s.a@gmail.com>
2020-07-23 22:44:30 -0400

    sandbox practice

--- test/sandbox.spec.js
+++ test/sandbox.spec.js
@@ -2,7 +2,21 @@ import SandPage from './SandPage';
 
 describe('SANDBOX', () => {
   it('should pass', () => {
-    browser.url('https://www.amazon.com/s?k=laptop&rh=n%3A565108&ref=nb_sb_noss');
-    SandPage.clickSearchItem(5)
+    browser.url('https://stage.localcoding.us/user/register');
+    $('#user_login_firstName').setValue('Test');
+    $('#user_login_lastName').setValue('Test');
+    $('#user_login_email').setValue(Math.random() + '@mail.com');
+    $('#user_login_password').setValue('123455');
+    $('#user_login_agreement').click();
+    $('button[type="submit"]').waitForClickable();
+    $('button[type="submit"]').click();
+    browser.pause(1500);
+  });
+
+  it('should ', function () {
+    $('.ant-select-selection-item').click();
+    browser.pause(1500);
+    $$('.ant-select-item-option-content')[5].click();
+    browser.pause(3000);
   });
 });
